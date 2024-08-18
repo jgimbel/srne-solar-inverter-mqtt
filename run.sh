dc6d4fff8c59672c062b04591bbe263961ebd8bd
root <root@ce11a32a-ssh.local.hass.io>
2024-08-18 14:57:10 -0400

    fix path. Fix permissions

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-/bin/bash -c 'source /opt/snre-solar-inverter-mqtt/venv/bin/activate && python snre-solar-inverter-mqtt.py'
+/bin/bash -c 'source /config/iot/srne-solar-inverter-mqtt/venv/bin/activate && python snre-solar-inverter-mqtt.py --dev /dev/serial/by-id/usb-1a86_USB_Serial-if00-port0'
 
