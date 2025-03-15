519ce1b1f8271fde541abd67913e464fdc50a871
root <root@ce11a32a-ssh.local.hass.io>
2025-03-15 15:56:56 -0400

    edits in the pi

--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
-/bin/bash -c 'source /config/iot/srne-solar-inverter-mqtt/venv/bin/activate && python snre-solar-inverter-mqtt.py --dev /dev/serial/by-id/usb-1a86_USB_Serial-if00-port0'
 
+#pip install minimalmodbus paho-mqtt
+
+python /config/iot/srne-solar-inverter-mqtt/snre-solar-inverter-mqtt.py --dev /dev/serial/by-id/usb-1a86_USB_Serial-if00-port0
--- snre-solar-inverter-mqtt.py
+++ snre-solar-inverter-mqtt.py
@@ -21,38 +21,38 @@ cmds = {
     'battery_voltage': {
         'cmd': (0x0101, 1, 3,True),
         'name': 'Battery Voltage',
-        'unit_of_measurement': 'v',
+        'unit_of_measurement': 'V',
         'device_class': 'voltage'
     },
     'battery_amps': {
         'cmd': (0x0102, 1, 3,True),
         'name': 'Battery Amps',
-        'unit_of_measurement': 'a',
+        'unit_of_measurement': 'A',
         'device_class': 'current',
         'value_template': '{{value | float * -1}}'
     },
     'shore_power_watts': {
         'cmd': (0x010e, 0, 3,True),
         'name': 'Power In',
-        'unit_of_measurement': 'w',
+        'unit_of_measurement': 'W',
         'device_class': 'power'
     },
     'solar_voltage': {
         'cmd': (0x0107, 1, 3,True),
         'name': 'Solar Voltage',
-        'unit_of_measurement': 'v',
+        'unit_of_measurement': 'V',
         'device_class': 'voltage'
     },
     'solar_amps': {
         'cmd': (0x0108, 1, 3,True),
         'name': 'Solar Amps',
-        'unit_of_measurement': 'a',
+        'unit_of_measurement': 'A',
         'device_class': 'current',
     },
     'solar_watts': {
         'cmd': (0x0109, 0, 3,True),
         'name': 'Solar Power',
-        'unit_of_measurement': 'w',
+        'unit_of_measurement': 'W',
         'device_class': 'power'
     },
     'charger_status': {
